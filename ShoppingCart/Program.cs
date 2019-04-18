3f061155e12d3a40e533776158305d2616a0e5c5
Lanping <lpluo@hotmail.com>
2019-04-18 12:09:57 +0100

    v2. check out for a shopping cart with offers and get its cost - Console app part. Missed to upload yesterday.

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -4,6 +4,7 @@ using ShoppingCart.Models;
 using ShoppingCart.Interfaces;
 using ShoppingCart.Checkout;
 using ShoppingCart.Checkout.Process;
+using ShoppingCart.Offers;
 
 namespace ShoppingCart
 {
@@ -13,6 +14,9 @@ namespace ShoppingCart
         {
             //v1. check out for a shopping cart and get its cost
             ShoppingCart();
+
+            //v2. check out for a shopping cart with offers and get its cost
+            ShoppingCartWithOffer();
 
         }
 
@@ -38,7 +42,34 @@ namespace ShoppingCart
 
         }
 
+        private static void ShoppingCartWithOffer()
+        {
+            IList<ICheckoutItem> _lstCheckItem = new List<ICheckoutItem>();
+            IList<IProduct> _lstProduct = new List<IProduct>();
+            //set up store products
+            _lstProduct = SetupProducts();
 
+            //set up offers
+            IList<KeyValuePair<int, OfferFlags>> lstProductOffer;
+            lstProductOffer = new List<KeyValuePair<int, OfferFlags>>()
+                { new KeyValuePair<int, OfferFlags>(1, OfferFlags.BuyOneGetOneFree),
+                new KeyValuePair<int, OfferFlags>(2, OfferFlags.ThreeForTwo) };
+
+            //check out items
+            var cart = new CheckoutProcesor(_lstCheckItem, _lstProduct);
+            var item = new CheckoutItem() { CheckoutItemId = 1, ProductId = 1, Quantity = 1 };
+            cart.AddItem(item);
+            item = new CheckoutItem() { CheckoutItemId = 2, ProductId = 2, Quantity = 1 };
+            cart.AddItem(item);
+            item = new CheckoutItem() { CheckoutItemId = 3, ProductId = 1, Quantity = 1 };
+            cart.AddItem(item);
+
+            //get the cost for the checkout items with the offers            
+            var tot = cart.GetTotalCost(lstProductOffer);
+
+            Console.WriteLine($"Total number of the items checked out with offers and the cost: {cart.GetTotalItems().Count}  {tot.ToString("C")}.");
+
+        }
 
         private static IList<IProduct> SetupProducts()
         {
